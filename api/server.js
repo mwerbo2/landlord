94c5ccd873d50257e92d4200d5bf70d70ca04a32
Michael W <mwerbo2@gmail.com>
2019-09-21 17:59:41 -0500

    Add in config for dev

--- api/server.js
+++ api/server.js
@@ -9,11 +9,11 @@ const app = express();
 app.use("/api/users", require("./routes/users"));
 
 // Serves static react filse from the build folder
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
